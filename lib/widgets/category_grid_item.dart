e121776ab912dd708cc023f73037143a7d1d2015
itsmartashub <coderawii@gmail.com>
2023-09-25 14:36:19 +0200

    08-006 making any widget tappable w `InkWell`

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,25 +9,32 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // koristimo Container widget jer nam on daje dosta opcija za bg color i bg decoration in general za ovaj widget
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    // Zelimo da items (child) budu tapables. Ali to mozemo i sa GestureDetector. Medjutim sa InkWell dobijemo isto sto i sa GestureDetector al ovde imamo i nice visually prikaz da se tapnulo
+    return InkWell(
+      onTap: () => {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      // koristimo Container widget jer nam on daje dosta opcija za bg color i bg decoration in general za ovaj widget
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
       ),
     );
   }
