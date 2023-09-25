3f1108af24449843c5896e36b04f7a453d4fa45f
itsmartashub <coderawii@gmail.com>
2023-09-25 15:50:56 +0200

    08-010 passing data to the target screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/models/category.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 import 'package:meal_app/screens/meals.dart';
 
@@ -10,14 +11,17 @@ class CategoriesScreen extends StatelessWidget {
   /* Ovo je prvi x da koristimo method u StatelessWidgetu. Ugl to bude u StatefullWidgetu jer kad koristimo method ugl apdejtujemo neki state. Medjutim ovde necemo apdejtovati state vec cemo loadovati drugi screen.
   I za to cemo koristiti feature koji je build-inovan u Flutter: Navigator.
   Posto smo u StatelessWidgetu, context NIJE GLOBALNO dostupan, zato moramo da u _selectCategory prihvatimo argument BuildContext context */
-  void _selectCategory(BuildContext context) {
-    print('💥💥💥💥💥💥 ALLALAL');
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     // Navigator.push(context, route); // nacin I
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     ); // nacin II
@@ -44,7 +48,7 @@ class CategoriesScreen extends StatelessWidget {
               CategoryGridItem(
                 category: category,
                 onSelectCategory: () {
-                  _selectCategory(context);
+                  _selectCategory(context, category);
                 },
               ),
           ],
