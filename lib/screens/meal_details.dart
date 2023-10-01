7feb408bba957a1cfe261db6c7f0807f9636ac20
itsmartashub <coderawii@gmail.com>
2023-10-01 16:50:30 +0200

    08-014 improving the`MealDetailsScreen`

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,11 +13,61 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
+
+      /* ? SingleChildScrollView or ListView
+      Da bi child bio scrollable zamenjujemo Column sa SingleChildScrollView ili ListView. Ali ne ListView.builder jer ovde necemo imati predugacku listu itema, vec samo ListView.
+      I sad jeste scrollable, ali su childovi poremeceni sad haha, vise nicu u centru kao kada su u Column widgetu. 
+      Zato cemo ipak vratiti Column i njega wrapovati u SingleChildScrollView widget */
+      // body: ListView(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14),
+
+            // uzvicnik je da kazemo dartu da ce to postojati
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            const SizedBox(height: 24),
+
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 14),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
