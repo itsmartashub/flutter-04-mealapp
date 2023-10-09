7ba0bb570629b888c1214fa4cb19b3496e4eea6d
itsmartashub <coderawii@gmail.com>
2023-10-09 15:20:16 +0200

    `09-013` connecting multiple providers w/ each other (dependent providers)

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:meal_app/providers/meals_provider.dart';
+
 // premestamo iz filters.dart ovde
 enum Filter {
   glutenFree,
@@ -30,4 +32,29 @@ class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
 
 final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
-        (ref) => FiltersNotifier());
+  (ref) => FiltersNotifier(),
+);
+
+/* Inace mozemo ofc da dodamo vise providera u istom fajlu, razdvajamo ih samo zarad lakseg snalazenja. Ali ako se vise provajdera baziru jedan na drugi, ima smisla da ih strpamo u isti fajl */
+final filteredMealsProvider = Provider((ref) {
+  /* zxakhvaljujuci ovome ref.watch(mealsProvider), riverpod ce da apdejtuje ovo dole meals.where kad god se watched vrednost promeni, tako da vracamo apdejtovane podatke. I onda svaki widget koji bi osluskivao filteredMealsProvider ce takodje biti apdejtovan ako je neki od dependency tog provajdera apdejtovan */
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+
+    return true;
+  }).toList();
+});
